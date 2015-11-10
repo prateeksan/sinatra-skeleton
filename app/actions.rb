b9656e04565db39c968d254ac9d8df597a75fcf2
vagrant <vagrant@lighthouse-labs>
2015-11-10 02:08:48 +0000

    Updated Project Without CSS

--- app/actions.rb
+++ app/actions.rb
@@ -9,3 +9,25 @@ get '/tracks' do
   erb :'tracks/index'
 end
 
+post '/tracks' do
+  @track = Track.new(
+    title: params[:title],
+    url: params[:url],
+    author: params[:author]
+    )
+  if @track.save
+    redirect '/tracks'
+  else
+    erb :'tracks/new'
+  end
+end
+
+get '/tracks/new' do
+  @track = Track.new
+  erb :'tracks/new'
+end
+
+get '/tracks/:id' do
+  @track = Track.find params[:id]
+  erb :'tracks/show'
+end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,5 @@
 class Track < ActiveRecord::Base
-
+  validates :author, presence: true
+  validates :title, presence: true
+  validates :url, presence:true
 end
